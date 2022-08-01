7bfea30de1c909a6e52bd10eb07779fe100fd50e
DiKaraProg <Di.kara.work@gmail.com>
2022-08-02 02:59:56 +0300

    Example_3

---
+++ Example_3/Animals/AnimalDataBase.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_3
+{
+    public class AnimalDataBase
+    {
+        private List<IAnimal> storage;
+        
+        public List<IAnimal> Storage { get => storage;set => storage = value; }
+
+        public AnimalDataBase() 
+        {
+            storage = new List<IAnimal>();
+        }
+        public void AddAnimal(IAnimal animal)
+        {
+            
+            if (!storage.Contains(animal))
+            {
+                
+                animal.Id = storage.Count+1;
+                storage.Add(animal);
+            }
+        }
+        public void RemoveAnimal(IAnimal animal)
+        {
+            int id = 0;
+            foreach (var item in storage)
+            {
+                if (item.Name == animal.Name)
+                {
+                    storage.Remove(item);
+                    break;
+                }
+            }
+            foreach (var item in storage)
+            {
+                id++;
+                item.Id = id;
+            }
+        }
+    }
+}
---
+++ Example_3/Interfaces/IBookSave.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_3
+{
+    public interface IBookSave
+    {
+        void SaveBookPages(List<IAnimal> animals);
+        
+    }
+}
